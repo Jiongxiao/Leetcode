bbfe7538ab55487b79845b9dc9bd8c493af29e13
Jiongxiao <tctjx@hotmail.com>
2016-05-16 00:41:56 +0800

    add leetcode 171

---
+++ 171_Excel_sheet_column_number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        size=len(s)
+        result=0
+        for i in range(size):
+        	result+=(ord(s[i])-ord('A')+1)*(26**(size-i))
+        return result
+
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
