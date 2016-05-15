cff0a0fbfd015c9b0e39192d77815e467fa90709
Jiongxiao <tctjx@hotmail.com>
2016-05-15 20:47:23 +0800

    modify leetcode 238

--- 238_Product_of_Array_Except_Self.py
+++ 238_Product_of_Array_Except_Self.py
@@ -15,3 +15,19 @@ class Solution(object):
         	temp*=nums[j]   # the multiplication of all the right nums of nums[j]
         return output
 
+class Solution(object):   # time limit exceeded!!!
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        size=len(nums)
+        output=[1]*size
+        for i in range(size):
+        	for j in range(size):
+        		if i==j:
+        			continue
+        		output[i]*=nums[j]
+        return output
+
+
