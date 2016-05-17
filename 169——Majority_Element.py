13a877950a17acef2723b89b62d5013587f23949
Jiongxiao <tctjx@hotmail.com>
2016-05-18 01:11:45 +0800

    modify leetcode 169

--- 169——Majority_Element.py
+++ 169——Majority_Element.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution(object):  #O(nlgn)
     def majorityElement(self, nums):
         """
         :type nums: List[int]
@@ -10,3 +10,23 @@ class Solution(object):
 
         sortedList=sorted(numSet.items(),key=lambda x: x[1],reverse=True)
         return sortedList[0][0]
+
+##高级方法 O(n)
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count=0
+        major=nums[0]
+        for num in nums:
+        	if num==major:
+        		count+=1
+        	else:
+        		count-=1
+        	if count<0:
+        		major=num
+        		count=1
+        return major
+
