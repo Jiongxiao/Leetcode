dd564783f4c0b551240728334932872d9955182d
Jiongxiao <tctjx@hotmail.com>
2016-05-18 12:59:29 +0800

    add leetcode 21

--- 21_Merge_Two_Sorted_Lists.py
+++ 21_Merge_Two_Sorted_Lists.py
@@ -11,3 +11,39 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+
+        if l1 is None or l2 is None:
+        	return l1 or l2
+        
+        list1=[]
+        list2=[]
+        while l1:
+        	list1.append(l1.val)
+        	l1=l1.next
+        while l2:
+        	list2.append(l2.val)
+        	l2=l2.next
+        array=[]
+        i=0;j=0
+        while i<len(list1) and j<len(list2):
+        	if list1[i]<list2[j]:
+        		array.append(list1[i])
+        		i+=1
+        	else:
+        		array.append(list2[j])
+        		j+=1
+        if i>=len(list1):
+        	array.extend(list2[j:])
+        else:
+        	array.extend(list1[i:])
+        cur=ListNode(0)
+        result=cur
+        for num in array:
+        	cur.next=ListNode(num)
+        	cur=cur.next
+        return result.next
+
+
+
+
+        	
