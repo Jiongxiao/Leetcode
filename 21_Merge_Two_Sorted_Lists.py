0543ee08c0c29eff8e898571abebd06fefb1dcc2
Jiongxiao <tctjx@hotmail.com>
2016-05-18 13:25:49 +0800

    modify leetcode 21

--- 21_Merge_Two_Sorted_Lists.py
+++ 21_Merge_Two_Sorted_Lists.py
@@ -44,6 +44,59 @@ class Solution(object):
         return head
 
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 is None or l2 is None:
+        	return l1 or l2
+        head=ListNode(0)
+        p=head
+        while l1 and l2:
+        	if l1.val<l2.val:
+        		p.next=ListNode(l1.val)
+        		l1=l1.next
+        		p=p.next
+        	else:
+        		p.next=ListNode(l2.val)
+        		l2=l2.next
+        		p=p.next
+        if l1:
+        	p.next=l1
+        else:
+        	p.next=l2
+        return head.next
 
+##更优化方法: 前两种方法可能不对，题目好像要求不能重建node
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if(l1 is None): return l2
+        if(l2 is None): return l1
+        if (l1.val<l2.val):
+        	l1.next=self.mergeTwoLists(l1.next,l2)
+        	return l1
+        else:
+        	l2.next=self.mergeTwoLists(l2.next,l1)
+        	return l2
         	
