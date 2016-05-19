6f19130392cc265e73e4d8d1e228bec38e6eb02c
Jiongxiao <tctjx@hotmail.com>
2016-05-19 14:09:50 +0800

    modify leetcode 326

--- 326_Power_of_Three.py
+++ 326_Power_of_Three.py
@@ -4,4 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        
+        if n==0:
+        	return False
+        import math
+        maxInteger=0xffffff
+        maxPower=int 
+        return math.pow(3,int(math.log(maxInteger,3))) % n==0
