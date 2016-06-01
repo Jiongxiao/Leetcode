751f88c8307af62a3d881c416c6e7244149766ca
Jiongxiao <tctjx@hotmail.com>
2016-06-01 09:21:25 +0800

    add leetcode 12

---
+++ 12_Integer_to_Roman.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        sta=[["","I","II","III","IV","V","VI","VII","VIII","IX"],\
+        ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"],\
+        ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],\
+        ["","M","MM","MMM"]]
+        base=[1000,100,10,1]
+        result=[]
+        for i in range(4):
+        	result.append(sta[3-i][num/base[i]])
+        return result
+
+
+
+
+
