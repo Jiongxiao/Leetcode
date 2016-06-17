eb9b6ed1c221a1e42a804be3193072731bd4a7eb
Jiongxiao <tctjx@hotmail.com>
2016-06-17 18:53:34 +0800

    leetcode 287

--- 287_Find_the_Duplicate_Number.py
+++ 287_Find_the_Duplicate_Number.py
@@ -12,4 +12,32 @@ class Solution(object):
         	buf=a
         return a
         ######这个方法非常牛逼，时间复杂度只要O(n)，但是题目要求不能改变list
-        
+
+
+##########O(lgn) 时间好像不是很好      
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def findD(start,end):
+            mid=(start+end)/2
+            m=0
+            l=0
+            s=0
+            for i in nums:
+                if start<=i and i <mid:
+                    s+=1
+                elif mid<i and i<=end:
+                    l+=1
+                elif mid==i:
+                    m+=1
+            if m>=2:
+                return mid
+            else:
+                if s>=l:
+                    return findD(start, mid-1)
+                else:
+                    return findD(mid+1, end)
+        return findD(1,len(nums)-1)
