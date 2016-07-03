b231f3a9f52c5434502d9c30c6c48c2613db5b7b
Jiongxiao <tctjx@hotmail.com>
2016-07-04 03:52:11 +0800

    leetcode 1

---
+++ 1_Two_sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic=dict()
+        for i in range(len(nums)):
+        	dic[nums[i]]=[]
+        for i in range(len(nums)):
+        	dic[nums[i]].append(i)
+        for i in dic.keys():
+        	index1=dic[i][0]
+        	if len(dic[i])<2:
+        		dic.pop(i)
+        	else:
+        		dic[i].pop(0)
+        	if (target-i) in dic:
+        		result=[index1,dic[target-i][0]]
+        return result
