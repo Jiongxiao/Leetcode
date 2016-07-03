1708470dcec4471e5c1be8368240100a117741de
Jiongxiao <tctjx@hotmail.com>
2016-07-04 01:37:55 +0800

    leetcode 349

---
+++ 349_Intersection_of_two_array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        dic1=dict();
+        result=[]
+        for i in nums1:
+        	dic1[i]=1
+        for j in nums2:
+        	if j in dic1:
+        		result.append(j)
+        return list(set(result))
