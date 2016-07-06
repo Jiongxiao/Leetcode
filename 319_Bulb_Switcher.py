0383707a65cc5aee580bee9ebcec33e8236323b9
Jiongxiao <tctjx@hotmail.com>
2016-07-07 02:06:16 +0800

    leetcode 319

---
+++ 319_Bulb_Switcher.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        bulb=[True]*(n+1)
+        for i in range(2,len(bulb)):
+        	for j in range(i,len(bulb)):
+        		if j%i==0:
+        			bulb[j]=not bulb[j]
+        result=-1
+        for i in bulb:
+        	if i==True:
+        		result+=1
+        return result
+######妥妥超时#######
+
