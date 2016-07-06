bbd709e249ed3d957b1b23c1a15674c06f43d0ce
Jiongxiao <tctjx@hotmail.com>
2016-07-07 02:34:41 +0800

    modify

--- 319_Bulb_Switcher.py
+++ 319_Bulb_Switcher.py
@@ -16,3 +16,12 @@ class Solution(object):
         return result
 ######妥妥超时#######
 
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        import math
+        return int(math.sqrt(n))
+###只有平方数是奇数个因数
