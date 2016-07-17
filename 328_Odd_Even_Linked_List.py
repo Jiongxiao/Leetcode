6da6e8884fc8e792b41c38bca5edace27e144136
Jiongxiao <tctjx@hotmail.com>
2016-07-17 13:06:54 +0800

    modify 328

--- 328_Odd_Even_Linked_List.py
+++ 328_Odd_Even_Linked_List.py
@@ -12,7 +12,7 @@ class Solution(object):
         """
         if not head or not head.next:
         	return head
-        odd=p=head
+        p=head
         even=q=head.next
         while(q.next):
             p.next=q.next
@@ -22,4 +22,23 @@ class Solution(object):
             if not q:
                 break
         p.next=even
-        return odd
+        return head
+
+######看上去好看点
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        p=head
+        even=q=head.next
+        while(q and q.next):
+            p.next=q.next
+            q.next=p.next.next
+            p=p.next
+            q=q.next
+        p.next=even
+        return head
