b68fd7b891d0444c092381de8ed93eb1231477b2
Jiongxiao <tctjx@hotmail.com>
2016-07-24 16:34:23 +0800

    leetcode 101

---
+++ 101_Symmetric_tree.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+        	return True
+        l=root.left
+        r=root.right
+        if not l and not r:
+        	return True
+        if l and r:
+        	if l.val==r.val:
+        		newroot1=l
+        		newroot1.right=r.right
+        		newroot2=r
+        		newroot2.r=l.right
+        		return self.isSymmetric(newroot2) and self.isSymmetric(newroot1)
+        	else:
+        		return False
+        return False
+
+########上面这个是不对的，引用的问题很容易被搞错！！！
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        import copy
+        if not root:
+        	return True
+        l=root.left
+        r=root.right
+        if not l and not r:
+        	return True
+        if l and r:
+        	if l.val==r.val:
+        		newroot1=copy.copy(l)
+        		newroot1.right=r.right
+        		newroot2=copy.copy(r)
+        		newroot2.right=l.right
+        		return self.isSymmetric(newroot2) and self.isSymmetric(newroot1)
+        	else:
+        		return False
+        return False
+       
