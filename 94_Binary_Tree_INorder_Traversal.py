be7a00648d9a3a89f634f64fb63dbbb0e27410ad
Jiongxiao <tctjx@hotmail.com>
2016-07-26 17:23:53 +0800

    modify

--- 94_Binary_Tree_INorder_Traversal.py
+++ 94_Binary_Tree_INorder_Traversal.py
@@ -48,3 +48,26 @@ class Solution(object):
         return result
 
 
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+        	return []
+        result=[]
+        from collections import deque
+
+        p=root
+        stack=deque()
+        while(stack or p):
+        	if p:
+        		stack.append(p)
+        		p=p.left
+        	else:
+        		p=stack.pop()
+        		result.append(p.val)
+        		p=p.right
+        return result
+
