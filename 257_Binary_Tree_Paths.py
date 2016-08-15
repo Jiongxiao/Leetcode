a8d0ceccde932105108b9c0579a191f2bdbc40a9
Jiongxiao <tctjx@hotmail.com>
2016-08-15 21:27:29 +0800

    modify

--- 257_Binary_Tree_Paths.py
+++ 257_Binary_Tree_Paths.py
@@ -76,7 +76,25 @@ class Solution:
 class Solution:  ##这玩意可以！
     # @param {TreeNode} root
     # @return {string[]}
-    def binaryTreePaths(self, root):		
+            
+    def binaryTreePaths(self, root):
+        if not root:
+            return []
+        from collections import deque
+
+        result=[]
+        stack=deque([(root,'')])
+        while(stack):
+            node, path=stack.pop()
+            if not node.left and not node.right:
+                result.append(path+str(node.val))
+            if node.right:
+                stack.append((node.right,path+str(node.val)+'->'))
+            if node.left:
+                stack.append((node.left, path+str(node.val)+'->'))
+        return result
+
+
 
 
 
