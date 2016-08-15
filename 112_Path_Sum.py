429248d284056a3579d2c317ee2e26bb0ae5defc
Jiongxiao <tctjx@hotmail.com>
2016-08-16 01:24:54 +0800

    modify

--- 112_Path_Sum.py
+++ 112_Path_Sum.py
@@ -5,7 +5,64 @@
 #         self.left = None
 #         self.right = None
 
-##########BFS
+##########BFS  
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        from collections import deque
+
+        queue=deque([(root,0)])
+        while(queue):
+            node,pathSum=queue.popleft()
+            pathSum+=node.val
+            if not node.left and not node.right:
+                if pathSum==sum:
+                    return True
+            if node.left:
+                queue.append((node.left,pathSum))
+            if node.right:
+                queue.append((node.right,pathSum))
+        return False
+
+
+#########DFS 
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        from collections import deque
+
+        stack=deque()
+        node=root
+        pathSum=0
+        while(True):
+            
+            while(node):
+                pathSum+=node.val
+                stack.append((node,pathSum))
+                node=node.left
+            if not stack:
+                return False
+            node,pathSum=stack.pop()
+            if not node.right and not node.left:
+                print pathSum
+                if pathSum==sum:
+                    return True
+            node=node.right
+
+#########DFS 
 class Solution(object):
     def hasPathSum(self, root, sum):
         """
@@ -24,12 +81,13 @@ class Solution(object):
             if not node.left and not node.right:
                 if pathSum==sum:
                     return True
-            if node.left:
-                stack.append((node.left,pathSum))
             if node.right:
                 stack.append((node.right,pathSum))
+            if node.left:
+                stack.append((node.left,pathSum))
         return False
 
 
-#########DFS
+
+
 
