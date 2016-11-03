13fb8932366451f162a22f9fc3603cacd09835c8
Jiongxiao <tctjx@hotmail.com>
2016-11-03 14:19:24 +0800

    leetcode 409

--- 409_Longest_Palindrome.py
+++ 409_Longest_Palindrome.py
@@ -4,4 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
+        if not s:
+        	return 0
+        dic=dict()
+        result=0
+        for i in s:
+        	if i in dic:
+        		result+=2
+        		del dic[i]
+        	else:
+        		dic[i]=1
+        if  dic:
+        	result+=1
+        return result
+
