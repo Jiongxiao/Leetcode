b8c8c8b0332a656f0a2758596c39d07d50438c53
Jiongxiao <tctjx@hotmail.com>
2016-11-22 15:28:11 +0800

    add

--- 198_House_Robber.py
+++ 198_House_Robber.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if not nums:
+            return 0
+        n=len(nums)
+        if n==1:
+            return nums[0]
+        last=0
+        now=0
+
+        for i in range(n):
+            temp=last
+            last=now
+            now=max(temp+nums[i],now)
+        return now
