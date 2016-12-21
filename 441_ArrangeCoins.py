06ab2e3b2f3a96891fb1ee75a0fc77b7cce62a0d
Jiongxiao <tctjx@hotmail.com>
2016-12-21 22:44:55 +0800

    leetcode441

---
+++ 441_ArrangeCoins.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        import math
+        result=0
+        root=int(math.sqrt(n))
+        for i in range(root,0,-1):
+            if i*(i+1)<=n:
+                result=i
+                return result
+
+        return result
