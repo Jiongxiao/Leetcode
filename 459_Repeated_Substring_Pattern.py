89924f2df7388e194e6ba103daa6a03d5193972c
Jiongxiao <tctjx@hotmail.com>
2016-12-26 23:21:43 +0800

    modify

--- 459_Repeated_Substring_Pattern.py
+++ 459_Repeated_Substring_Pattern.py
@@ -4,3 +4,35 @@ class Solution(object):
         :type str: str
         :rtype: bool
         """
+
+        front=1
+        n=len(str)
+        first=str[0]
+        while True:
+            while front<n:
+                if str[front]!=first or n%(front)!=0:
+                    front+=1
+                else: break
+            if front>=n:
+                return False
+            for i in range(1,n/front):
+                if str[0:front]!=str[i*front:(i+1)*front]:
+                    front+=1
+                    break
+                if i==n/front-1:
+                    return True
+
+
+class Solution(object):   ##reference
+    def repeatedSubstringPattern(self, str):
+        """
+        :type str: str
+        :rtype: bool
+        """
+        ss=(str+str)[1:-1]
+        return ss.find(str)!=-1
+
+
+
+
+        
