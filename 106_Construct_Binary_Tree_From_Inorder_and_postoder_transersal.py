b16899369dc7f2f2820bdae47800d09d5fa39937
Jiongxiao <tctjx@hotmail.com>
2016-12-27 22:58:02 +0800

    leetcode 106

--- 106_Construct_Binary_Tree_From_Inorder_and_postoder_transersal.py
+++ 106_Construct_Binary_Tree_From_Inorder_and_postoder_transersal.py
@@ -5,6 +5,25 @@
 #         self.left = None
 #         self.right = None
 
+class Solution(object): ## memory exceed!!!  ??????why   postorder[0:i] too many times vs postorder always the original one
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        if not postorder:
+            return None
+        root=TreeNode(postorder[-1])
+        n=len(inorder)
+        for i in range(n):
+            if inorder[i]==root.val:
+                break
+        root.left=self.buildTree(inorder[0:i],postorder[0:i])
+        root.right=self.buildTree(inorder[i+1:],postorder[i:-1])
+        return root
+
+
 class Solution(object):
     def buildTree(self, inorder, postorder):
         """
@@ -12,4 +31,13 @@ class Solution(object):
         :type postorder: List[int]
         :rtype: TreeNode
         """
+        if not postorder or not inorder:
+            return None
+        root=TreeNode(postorder.pop())
+        i=inorder.index(root.val)
+        root.right=self.buildTree(inorder[i+1:],postorder)
+        root.left=self.buildTree(inorder[0:i],postorder)
+        return root
+
+
         
