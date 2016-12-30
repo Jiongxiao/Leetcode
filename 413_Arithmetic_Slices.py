d2148c1e2a5fc37fb0ab8b4a1d43e7bfcb51250b
Jiongxiao <tctjx@hotmail.com>
2016-12-30 17:28:22 +0800

    modify

--- 413_Arithmetic_Slices.py
+++ 413_Arithmetic_Slices.py
@@ -4,3 +4,17 @@ class Solution(object):
         :type A: List[int]
         :rtype: int
         """
+        n=len(A)
+        if n<3:
+            return 0
+        res=0
+        tail=[0]*n
+        if A[2]+A[0]==A[1]<<1:
+            tail[2]=1
+            res+=1
+
+        for i in range(3,n):
+            if A[i]+A[i-2]==A[i-1]<<1:
+                tail[i]=tail[i-1]+1
+                res+=tail[i]
+        return res
