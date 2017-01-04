7aa625a18b24b9a99a2078ff59b998f1c07458c6
Jiongxiao <tctjx@hotmail.com>
2017-01-04 16:40:31 +0800

    modify

--- 445_Add_Two_Numbers_II.java
+++ 445_Add_Two_Numbers_II.java
@@ -57,7 +57,48 @@ public class Solution {
         if (resi>0) {ListNode cur=new ListNode(resi);
             cur.next=front;
             front=cur;}
-            
+
             return front;
         }
     }
+
+// Stack
+
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            Stack <Integer> s1=new Stack<>();
+            Stack <Integer> s2=new Stack<>();
+            while(l1!=null){
+                s1.push(l1.val);
+                l1=l1.next;
+            }
+            while(l2!=null){
+                s2.push(l2.val);
+                l2=l2.next;
+            }
+            int sum=0;
+            ListNode front=null;
+            while(!s1.empty() || !s2.empty()){
+                if (!s1.empty()) sum+=s1.pop();
+                if (!s2.empty()) sum+=s2.pop();
+                ListNode cur=new ListNode(sum%10);
+                cur.next=front;
+                front=cur;
+                sum=sum/10;
+            }
+            if (sum>0){
+                ListNode cur=new ListNode(sum);
+                cur.next=front;
+                front=cur;
+            }
+            return front;
+    }
+}
