334c106aebc1c9eef352b9a8e711e00a2b0c36cd
Jiongxiao <tctjx@hotmail.com>
2017-01-06 15:52:05 +0800

    leetcode 447

---
+++ 447_Number_Of_Boomeranges.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        int n=points.length;
+        int res=0;
+        if (n<3) return 0;
+        for (int i=0;i<n;i++){
+            Map <Integer,Integer> map= new HashMap<>();
+            for (int j=0; j<n; j++){
+                int dis=(points[i][0]-points[j][0])*(points[i][0]-points[j][0])
+                +(points[i][1]-points[j][1])*(points[i][1]-points[j][1]);
+                map.put(dis,map.getOrDefault(dis,0)+1);
+            }
+            Iterator iter=map.entrySet().iterator();
+            while(iter.hasNext()){
+                Map.Entry entry=(Map.Entry) iter.next();
+                int val= (int) entry.getValue();
+                // System.out.println(val);
+                if (val>=2){
+                    res+=val*(val-1);
+                }
+            }
+        }
+
+        return res;
+    }
+}
