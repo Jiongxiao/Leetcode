73462f29f13dc121007babefda6b8d5dee11fd66
Jiongxiao <tctjx@hotmail.com>
2017-01-06 15:57:40 +0800

    modify

--- 447_Number_Of_Boomeranges.java
+++ 447_Number_Of_Boomeranges.java
@@ -24,3 +24,25 @@ public class Solution {
         return res;
     }
 }
+
+
+public class Solution{
+    public int numberOfBoomerangs(int[][] points){
+        int n= points.length;
+        int res=0;
+        if (n<3) return 0;
+        Map <Integer,Integer> map= new HashMap<>();
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                int dis=(points[i][0]-points[j][0])*(points[i][0]-points[j][0])
+                +(points[i][1]-points[j][1])*(points[i][1]-points[j][1]);
+                map.put(dis,map.getOrDefault(dis,0)+1);
+            }
+            for (int val: map.values()){
+                res+=val*(val-1);
+            }
+            map.clear();
+        }
+        return res;
+    }
+}
