1dab89c1fcbb4775775725468528f42fc6880ef6
Jiongxiao <tctjx@hotmail.com>
2017-01-18 22:25:02 +0800

    leetcode 437

--- 437_Path_Sum_III.java
+++ 437_Path_Sum_III.java
@@ -9,6 +9,12 @@
  */
 public class Solution {
     public int pathSum(TreeNode root, int sum) {
-        
+        if (root==null) return 0;
+        return helper(root,sum)+pathSum(root.left,sum)+pathSum(root.right,sum);
+    }
+    public int helper(TreeNode node, int sum){
+        if (node==null) return 0;
+        return (node.val==sum ? 1:0)+ helper(node.left,sum-node.val)
+        +helper(node.right,sum-node.val);
     }
 }
