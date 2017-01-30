5ee5f6fd1e29c21f69b8bf8d7929130948e0c60e
Jiongxiao <tctjx@hotmail.com>
2017-01-31 01:51:51 +0800

    modify

--- 437.py
+++ 437.py
@@ -12,3 +12,23 @@ class Solution(object):
         :type sum: int
         :rtype: int
         """
+        if not root:
+            return 0
+        res=dict()
+        from collections import deque
+        st=deque()
+        st.append([root,{-3000000:1}])
+        while(st):
+            node,map1=st.pop()
+            if node:
+                new_map=dict()
+                for key in map1.keys():
+                    new_map[key+node.val]=map1.get(key)
+                new_map[node.val]=new_map.get(node.val,0)+1
+                for key in new_map.keys():
+                    res[key]=res.get(key,0)+new_map[key]
+                    # print key,new_map[key]
+                st.append([node.right,new_map])
+                st.append([node.left,new_map])
+        return res.get(sum,0)
+
