62d659b73b1b418739f04550a3d2793f5fa174d8
Jiongxiao <tctjx@hotmail.com>
2017-02-16 01:22:58 +0800

    modify

--- 153_Find_Minimum_in_Rotated_Sorted_Array.py
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -4,3 +4,37 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        n=len(nums)
+        if n==1:
+            return nums[0]
+        left=0
+        right=n-1
+        while True:
+            if left==right:
+                return nums[left]
+            mid=(left+right)/2
+            if nums[mid]>=nums[left] and nums[mid]>=nums[right]:
+                left=mid+1
+            elif nums[left]>nums[right]:
+                left=left
+            else: return nums[left]
+
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        if n==1:
+            return nums[0]
+        left=0
+        right=n-1
+        while left<right:
+            mid=(left+right)/2
+            if nums[mid]<nums[right]:
+                right=mid
+            else:
+                left=mid+1
+        return nums[left]
