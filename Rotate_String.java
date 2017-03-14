062df69a99d2ccbc3ff6940ca8a53647f45b6737
Jiongxiao <tctjx@hotmail.com>
2017-03-15 01:30:33 +0800

    modify

--- Rotate_String.java
+++ Rotate_String.java
@@ -20,3 +20,30 @@ public class Solution {
         str[0] = temp;
     }
 }//////////////////超时
+
+public class Solution {
+    /**
+     * @param str: an array of char
+     * @param offset: an integer
+     * @return: nothing
+     */
+    public void rotateString(char[] str, int offset) {
+        // write your code here
+        int n = str.length;
+        if (n < 2) return;
+        offset = offset % n;
+        rotate(str,0,n - offset - 1);
+        rotate(str,n - offset, n-1);
+        rotate(str,0,n-1);
+    }
+    
+    public void rotate(char[] str, int start, int end){
+        while(start < end){
+            char temp = str[end];
+            str[end] = str[start];
+            str[start] = temp;
+            start++;
+            end--;
+        }
+    }
+}
